bc59ce082f1a43c1e61683a1bda3856977087488
Michael Blake <mikeblake@hotmail.com>
2012-12-13 19:30:42 +0000

    First version

---
+++ BabyBet/Controllers/BetController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BabyBet.Controllers
+{
+    public class BetController : Controller
+    {
+        //
+        // GET: /Bet/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BabyBet/Models/Bet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BabyBet.Models
+{
+    public class Bet
+    {
+        public DateTimeOffset BirthDate {get ; set;}
+        public float Weight { get; set; }
+        public bool isMale { get; set; }
+        public string Name { get; set; }
+    }
+}
